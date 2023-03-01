eca1400b3de123e17e8bbf39ef75900bcd963325
JosuaMnl <josuamnalu@gmail.com>
2023-03-01 17:53:35 +0700

    Explicit Intent

---
+++ app/src/main/java/com/yosha10/explicit_implicit_intent_kotlin/MoveActivity.kt
@@ -0,0 +1,11 @@
+package com.yosha10.explicit_implicit_intent_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
